3946b0ef3c687ecf8361447febc97112b30c797d
zamharrad <mhmdzamhar@gamil.com>
2020-04-07 12:38:55 +0530

    add ui changes

--- src/Pages/homepage/HomePage.js
+++ src/Pages/homepage/HomePage.js
@@ -1,37 +1,16 @@
 import React from "react";
 import "./Homepage.scss";
+import MenuItem from "../../Components/Menu-Item/MenuItem";
 
 function HomePage() {
   return (
     <div className="homepage">
       <div className="directory-menu">
-        <div className="menu-item">
-          <div className="item-content">
-            <h1 className="title">HAT</h1>
-            <span className="sub-title">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="item-content">
-            <h1 className="title">HAT</h1>
-            <span className="sub-title">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="item-content">
-            <h1 className="title">HAT</h1>
-            <span className="sub-title">SHOP NOW</span>
-          </div>
-        </div>
-
-        <div className="menu-item">
-          <div className="item-content">
-            <h1 className="title">HAT</h1>
-            <span className="sub-title">SHOP NOW</span>
-          </div>
-        </div>
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
       </div>
     </div>
   );
