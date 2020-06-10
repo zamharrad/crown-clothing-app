80f258a5df868fb46e33ecc640a56c5a769991f9
zamharrad <mhmdzamhar@gamil.com>
2020-06-10 14:25:14 +0530

    commit changes

--- src/Components/Menu-Item/MenuItem.js
+++ src/Components/Menu-Item/MenuItem.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./MenuItem.scss";
 
-function MenuItem() {
-  return (
-    <div className="menu-item">
-      <div className="item-content">
-        <h1 className="title">HAT</h1>
-        <span className="sub-title">SHOP NOW</span>
-      </div>
+const MenuItem = ({title}) => (
+  <div className="menu-item">
+    <div className="item-content">
+      <h1 className="title">{title}</h1>
+      <span className="sub-title">SHOP NOW</span>
     </div>
-  );
-}
+  </div>
+);
 
 export default MenuItem;
