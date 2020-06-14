5dee7f076966597f0b5c9c38d7b3961b8adb4f37
zamharrad <mhmdzamhar@gamil.com>
2020-06-14 17:35:03 +0530

    add shop data

---
+++ src/Pages/shoppage/ShopPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import SHOP_DATA from "./shopData";
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+  render() {
+    return <div>Shop Page</div>;
+  }
+}
+
+export default ShopPage;
